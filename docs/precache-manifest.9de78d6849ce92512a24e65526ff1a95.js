d1f9db8b529164894cb4f72d73e10155ee61a868
Carlos Morales <carlosjosuemn98@gmail.com>
2020-09-27 20:21:12 -0500

    add docs

---
+++ docs/precache-manifest.9de78d6849ce92512a24e65526ff1a95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a77b22fcac36528367caef077af6d866",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86a277c03ba899cf65aa",
+    "url": "/static/css/main.c522aa81.chunk.css"
+  },
+  {
+    "revision": "b5317474f24c348574ab",
+    "url": "/static/js/2.4a935a37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4a935a37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86a277c03ba899cf65aa",
+    "url": "/static/js/main.bd0457b2.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  }
+]);
